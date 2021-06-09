fd68453893e48d317aff5b094a4c629539b06a46
Tony Kwok <tnrzk13@gmail.com>
2021-06-09 14:47:09 -0400

    added favourites table migration and seed

---
+++ database/migrations/20210609183416_add-favourites-table.js
@@ -0,0 +1,15 @@
+exports.up = function (knex) {
+    return knex.schema.createTable("favourites", function (table) {
+      table.uuid("id").notNullable().primary();
+      table.uuid("user_id").notNullable();
+      table.foreign("user_id").references("users.id");
+      table.uuid("post_id").notNullable();
+      table.foreign("post_id").references("posts.id");
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+    });
+  };
+  
+  exports.down = function (knex) {
+    return knex.schema.dropTable("favourites");
+  };
+  
