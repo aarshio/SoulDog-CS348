8ce9effc975db3773a0ff75bafa73a5b0adca52b
Aarsh <aarsh937@gmail.com>
2021-06-11 15:42:54 -0400

    test-sample

---
+++ database/test-sample.sql
@@ -0,0 +1,26 @@
+INSERT INTO users (id, username, PASSWORD, email) VALUES("1", "user1", "user1password", "user1@gmail.com");
+
+SELECT * FROM users WHERE username = "user1" AND password = "user1password";
+
+UPDATE users SET bio = "Looking for my dream dog", energy = "Low", age = 60, backyard = "Small", current_dog = "No", free_time = "High", Strength = "Medium" WHERE id = "1";
+
+SELECT * FROM pets WHERE aggression="Low" AND energy="Medium" AND maintenance="Medium";
+
+DELETE FROM favourites WHERE id = "aehdkbc82n-ddi29d-wddn";
+
+INSERT INTO comments (id, text, user_id, post_id) VALUES ("1", "Love this dog", "1", "150e2144-ca4b-4770-a898-6be09aeb69d7");
+
+INSERT INTO likes (id, user_id, post_id) VALUES (1010, 1234, 1111);
+
+INSERT INTO posts (id, user_id, pet_id, title, content, name, age) VALUES ("826383bid2", "jdhe83-fi3", "gd2n-dubwjd", "buy my dog", "cool content", "Doggo", 10);
+
+DELETE FROM posts WHERE id="826383bid2";
+
+-- DISPLAY TABLES 
+
+SELECT * FROM users;
+SELECT * FROM pets;
+SELECT * FROM posts;
+SELECT * FROM comments;
+SELECT * FROM likes;
+SELECT * FROM favourites;
